e82157aad70d0a7b7b90932935942130a13f332d
Viktor3RDart <fable198666@mail.ru>
2023-05-18 19:43:40 +0300

    add main with sum, minus etc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите число:");
+        int number1 = new Scanner(System.in).nextInt();
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: " + (number1 + number2));
+        System.out.println("Разность чисел: " + (number1 - number2));
+        System.out.println("Произведение чисел: " + (number1 * number2));
+        System.out.println("Деление чисел: " + ((double) number1 / number2));
     }
 }
