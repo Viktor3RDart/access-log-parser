0f183b98fbf07e282e12f7d96e0219cdd44ac1d0
Viktor3RDart <fable198666@mail.ru>
2023-05-18 19:54:32 +0300

    add main with sum, minus etc.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число:");
+        System.out.println("Введите первое число:");
         int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
         int number2 = new Scanner(System.in).nextInt();
         System.out.println("Сумма чисел: " + (number1 + number2));
         System.out.println("Разность чисел: " + (number1 - number2));
