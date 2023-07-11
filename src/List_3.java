377f06ff7c9118396f1160c5b830e56e36cc2f69
Viktor3RDart <fable198666@mail.ru>
2023-07-11 08:00:39 +0300

    Hw8 task1,task2,task3 done

--- src/List_3.java
+++ src/List_3.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //Перед вами код метода, который переставляет числа в массиве в обратном порядке.
 //
@@ -12,6 +13,27 @@ import java.util.ArrayList;
 //        }
 //        Задача: написать такой же метод для ArrayList<Integer>.
 public class List_3 {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = (int) (Math.random() * (200 + 1)) - 100;
+        }
+        // Показать первоначальный массив int[] intArray
+        System.out.println("Массив int[] intArray: " + Arrays.toString(intArray));
+        ArrayList<Integer> intList = new ArrayList<>();
+        for (int j : intArray) {
+            intList.add(j);
+        }
+        // Показать первоначальный ArrayList<Integer> intList
+        System.out.println("Лист ArrayList<Integer> intList: " + intList + "\n");
+        // Показать отсортированный массив int[] intArray
+        reverse(intArray);
+        System.out.println("Сортировка массива: " + Arrays.toString(intArray));
+        // Показать отсортированный массив ArrayList<Integer> intList
+        reverse2(intList);
+        System.out.println("Сортировка листа:   " + intList);
+    }
+
     public static void reverse(int[] intArray) {
         int n = intArray.length - 1;
         for (int i = 0; i < intArray.length / 2; i++) {
@@ -20,12 +42,13 @@ public class List_3 {
             intArray[n - i] = temp;
         }
     }
+
     public static void reverse2(ArrayList<Integer> intList) {
         int n = intList.size() - 1;
         for (int i = 0; i < intList.size() / 2; i++) {
             int temp = intList.get(i);
-            intList.get(i) = intList.get(n - i);
-            intList.get(n - i) = temp;
+            intList.set(i, intList.get(n - i));
+            intList.set((n - i), temp);
         }
     }
 }
